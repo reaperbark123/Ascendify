7c9eec9a12f193ee2277cd771065b0bd39c04a93
reaperbark123 <88252453+reaperbark123@users.noreply.github.com>
2026-09-01 17:28:50 -0500

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -193,7 +193,7 @@ local SaveManager = {} do
 				return self.Library:Notify('Failed to load autoload config: ' .. err)
 			end
 
-			-- self.Library:Notify(string.format('Auto loaded config %q', name))
+			self.Library:Notify(string.format('Auto loaded config %q', name))
 		end
 	end
 
